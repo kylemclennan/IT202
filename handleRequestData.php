23e461083e108ad8fc200d00f1046adc03e741f4
kylemclennan <km653@njit.edu>
2019-09-30 04:04:38 -0400

    adding .gitignore

---
+++ handleRequestData.php
@@ -0,0 +1,18 @@
+<?php
+	echo "<pre>" . var_export($_GET, true) . "</pre>";
+
+	if(isset($_GET['name'])){
+		echo "<br>Hello, " . $_GET['name'] . "<br>";
+	}
+	if(isset($_GET['number'])){
+		$number = $_GET['number'];
+		echo "<br>" . $number . " should be a number ...";
+		echo "<br>but it might not be<br>";
+	}
+	//TODO
+	//handle addition of 2 or more parameters with proper numbering
+	//concatenate 2 or more parameters values and echo them
+	//try passing multiple same-named parameters with different values
+	//try passing a parameter value with special characters
+	//web.njit.edu/km653/IT202/handleRequestData.php?parameter1=a&p2=b
+?>
--- inidb.php
+++ inidb.php
@@ -5,12 +5,32 @@ ini_set('display_startup_errors',1);
 error_reporting(E_ALL);
 //pull in config.php so we can access variables from it
 require('config.php');
-echo "Loaded Host: " .  $host;
+//echo "Loaded Host: " .  $host;
 
 $connection_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
 
 try{
+	foreach(glob("sql/*.sql") as $filename){
+		//echo $filename;
+		$sql[$filename] = file_get_contents($filename);
+		//echo $sql[filename];
+	}
+	ksort($sql);
+	//connect db
 	$db = new PDO($connection_string, $username, $password);
+	//$db-> setAttribute(PDO::ATTR_ERRMODE);
+	foreach($sql as $key => $value){
+		echo "<br>Running: " . $key;
+		$stmt = $db->preare($value);
+		$result = $stmt->execute();
+		$error = $stmt->errorInfo();
+		if($error && $error[0] !== '0000'){
+			echo "<br>Error:<pre>" . var_export($error,true) . "</pre><br>";
+		}
+		echo "<br>$key result " . ($result>0?"Success":"Fail") . "<br>;
+	}
+	
+	/*
 	echo "Should have connected";
 	//create table
 	$query = "create table if not exists `TestUsers`(
@@ -22,17 +42,21 @@ try{
 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 	$stmt = $db->prepare($query);
 	print_r($stmt->errorInfo());
-	r$ = $stmt->execute();
+	$r = $stmt->execute();
 	echo "<br>" . ($r>0?"Created table or already exists":"Failed to create table") . "<br>";
 	unset($r);
 	//simple insert
 	//TODO homework make values varables bindable
-	$insert_query = "INSERT INTO `TestUsers` ( `username`, `pin`) VALUES (`JohnDoe`, 1234)";
+	$insert_query = "INSERT INTO `TestUsers` ( `username`, `pin`)
+		VALUES (:username, :pin)";
 	$stmt = $db->prepare($insert_query);
-	$r = $stmt->execute();
+	$newUser = "Billy";
+	$newPin = 1234;
+	$r = $stmt->execute(array(":username"=> $newUser, ":pin"=>newPin));
+	print_r($stmt->errorInfo());
 	//TODO catch error from DB
 	echo "<br>" . ($r>0?"INSERT successful":"InsertFailed") . "<br>";
-
+*/
 }
 catch(Exception $e){
 	echo $e->getMessage();
