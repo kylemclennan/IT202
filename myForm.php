7d3ddcb20192daa99b09d4857b0d7b704450963b
kylemclennan <km653@njit.edu>
2019-10-07 11:03:44 -0400

    adding .gitignore

---
+++ myForm.php
@@ -0,0 +1,38 @@
+<?php
+//helper
+function getName(){
+	if(isset($_GET['name'])){
+		echo "<p>Hello, " . $_GET['name'] . "</p>";
+	}
+}
+?>
+<html>
+<head></head>
+<body><?php getName();?>
+<form mode="GET" action="#">
+<input name="name" type="text" placeholder="Enter your name"/>
+<input name="password" type="password" placeholder="Enter your password"/>
+
+<label for ="Yes">Yes</label><!-- for="id of element"-->
+<input type=:"radio" name="radio" id="Yes" calue="Yes"/>
+<label for"No>No</label>
+<input> type="radio" name="radio" id="No" value="No"/>
+<!--comment-->
+<select name ="dropdown">
+	<option value="1">One</option>
+	<option value="2">Two</option>
+	<option value="3"Three</option>
+</select>
+<input type="checkbox" name"check"/>
+<textarea name="text"></textarea>
+<input type="date" name="date"/>
+<!--end-->
+
+<input type="submit" value="Try it"/>
+</form>
+</body>
+</html>
+<?php
+if(isset($_GET)){
+	echo "<br><pre>" . var_export($_GET, true) . "</pre><br>";
+}
